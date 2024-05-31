3ce2cdff41862440c7ee46f0dda14d17b076cae7
Weslei Peter <wesleipeter@hotmail.com>
2024-05-31 14:39:41 -0300

    Commit second

--- src/com/weslei/curso_dio_variaveis/App.java
+++ src/com/weslei/curso_dio_variaveis/App.java
@@ -2,6 +2,7 @@ package com.weslei.curso_dio_variaveis;
 
 public class App {
   public static void main(String[] args) {
+    // modifcar arquivo.
     System.out.println("Criando exemplo para o GIT");
   }
 }
